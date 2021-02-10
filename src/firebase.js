17778b4d82d7230c309cc2154783aa7bd02a3567
Siddhesh Kankekar <kankekas@tcd.ie>
2021-02-10 23:19:21 +0000

    Initial Project setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpFM5EDMoSZ6bkE2VE6DxwYE7HyataYRk",
+  authDomain: "project-facebook-a52db.firebaseapp.com",
+  projectId: "project-facebook-a52db",
+  storageBucket: "project-facebook-a52db.appspot.com",
+  messagingSenderId: "146848540372",
+  appId: "1:146848540372:web:7abf3401d462ebecfdfd41",
+  measurementId: "G-NEP9LEYGRC",
+};
